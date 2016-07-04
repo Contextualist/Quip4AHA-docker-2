f756260cd7b22c904faa1534f2718b2bbc549d9b
Contextualist <harzjc@gmail.com>
2016-07-04 19:23:34 +0800

    Minor simplifications

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import traceback
+from traceback import format_exc
 from time import sleep
-from functools import partial
-import threading
-import schedule
+from threading import Thread
+from schedule import Scheduler
 
 from NewDoc import NewDoc
 from AssignHost import AssignHost
@@ -33,24 +32,24 @@ def updateweather():
 
 @app.errorhandler(Exception)
 def handle_exception(e):
-    tb = traceback.format_exc()
+    tb = format_exc()
     app.logger.error(tb)
-    return "<pre>%s</pre>" % (tb), e.code if 'code' in dir(e) else 500
+    return "<pre>%s</pre>" % tb, e.code if hasattr(e,'code') else 500
 
 
 
-class Scheduler(schedule.Scheduler):
+class Scheduler4AHA(Scheduler):
     
     def __init__(self):
-        schedule.Scheduler.__init__(self)
+        Scheduler.__init__(self)
         fc = app.test_client() # Flask client emulator
         # Schedule uses the local timezone, i.e. CST for DaoCloud.
-        self.every().friday.at("16:10").do(partial(fc.get,'/newdoc'))
-        self.every().sunday.at("07:27").do(partial(fc.get,'/updateweather'))
-        self.every().wednesday.at("07:27").do(partial(fc.get,'/updateweather'))
+        self.every().friday.at("16:10").do(fc.get,'/newdoc')
+        self.every().sunday.at("07:27").do(fc.get,'/updateweather')
+        self.every().wednesday.at("07:27").do(fc.get,'/updateweather')
     
     def run_alongside(self, interval=1):
-        class ScheduleThread(threading.Thread):
+        class ScheduleThread(Thread):
             @classmethod
             def run(cls):
                 while 1:
@@ -62,6 +61,6 @@ class Scheduler(schedule.Scheduler):
         
 
 if __name__ == '__main__':
-    cron = Scheduler()
+    cron = Scheduler4AHA()
     cron.run_alongside(interval=60)
     app.run(debug=False, host='0.0.0.0', port=80)
